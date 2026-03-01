1ad256994543ebee5a8b0b025a7f67a7ab58c33b
Ke'Yonna Bass <48772944+Keyonna99@users.noreply.github.com>
2026-02-28 21:51:23 -0600

    Delete download_model.py

--- download_model.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url = "https://github.com/chuanqi305/MobileNet-SSD/raw/master/mobilenet_iter_73000.caffemodel"
-output = "models/mobilenet_iter_73000.caffemodel"
-
-print("Downloading model...")
-urllib.request.urlretrieve(url, output)
-print("Download complete!")
